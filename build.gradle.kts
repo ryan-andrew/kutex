a677fcb24d8551f9ea6d11f9eee895d0a86af9df
Ryan <ryan@ryanandrew.dev>
2022-12-24 21:50:03 -0600

    Delete old docs of the same version

--- build.gradle.kts
+++ build.gradle.kts
@@ -139,6 +139,7 @@ tasks.dokkaHtml.configure {
 //            }
 //        }
 //    }
+    tmpDocDir.listFiles()?.forEach { println("~~~~~~~${it.name}") }
     tmpDocDir.listFiles()?.filter { it.name == projectVersion }?.forEach { it.delete() }
     pluginConfiguration<org.jetbrains.dokka.versioning.VersioningPlugin, org.jetbrains.dokka.versioning.VersioningConfiguration> {
         version = projectVersion
